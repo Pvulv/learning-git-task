b71892e1c0debe044eba37fa079e731294f778c6
Paulina Wróbel <paulinawro13579@gmail.com>
2024-10-18 18:05:14 +0200

    loop and capitalize

--- dict_shop.py
+++ dict_shop.py
@@ -2,3 +2,7 @@ shops = {
     "piekarnia" : ["chleb", "bułki", "pączek"],
     "warzywniak" : ["marchew", "seler", "rukolę"]
 }
+print("Lista zakupów")
+for shop, products in shops.items():
+    capitalized_products = [product.capitalize() for product in products]
+    print("idę do", shop.capitalize(), "i kupuję", capitalized_products)
