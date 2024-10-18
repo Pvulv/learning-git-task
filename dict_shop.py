051b437ec379a3450f62d40f50fe664fcb62a777
Paulina Wróbel <paulinawro13579@gmail.com>
2024-10-18 18:06:16 +0200

    Sum up

--- dict_shop.py
+++ dict_shop.py
@@ -6,3 +6,6 @@ print("Lista zakupów")
 for shop, products in shops.items():
     capitalized_products = [product.capitalize() for product in products]
     print("idę do", shop.capitalize(), "i kupuję", capitalized_products)
+
+Suma = sum(len(product) for product in shops.values())
+print("W sumie kupuję", Suma, "produktów")
